bd47c981ea664155d1e88db1d49e4a4a9667c45e
Limorwin <limor12@student.ubc.ca>
2025-12-02 15:39:27 -0800

    read-data-script

---
+++ scripts/download_data.py
@@ -0,0 +1,33 @@
+# download_data.py
+# author: Limor Winter
+# date: 2025-12-02
+
+import click
+import requests
+import zipfile
+import os
+
+@click.command()
+@click.option('--url', type=str, help="URL of dataset to be downloaded")
+
+def main(url):
+    """Download and extract the UCI Mushroom dataset."""
+    response = requests.get(url)
+    with open("../data/raw/mushroom.zip", 'wb') as f:
+        f.write(response.content)
+    with zipfile.ZipFile("../data/raw/mushroom.zip", 'r') as zip_ref:
+        zip_ref.extractall("../data/raw")
+
+        
+if __name__ == '__main__':
+    main()
+    
+
+# @click.option('--data_path', type=str, help="Path to the data file to check")
+
+# correct_exts = {".csv",".data"}
+# ext = os.path.splitext(data_path)[1].lower()
+# assert ext in correct_exts, (
+#     f"[File format] '{data_path}' has extension '{ext}', "
+#     f"expected one of {sorted(correct_exts)}."
+# )
